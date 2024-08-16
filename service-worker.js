23de8e96d1835de40751123992b67e9804b4042b
Júlia <juliaeeo@gmail.com>
2024-08-16 16:36:41 -0300

    PWA insertion

---
+++ service-worker.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = "eshelf-cache-v1";
+const urlsToCache = [
+  "/",
+  "/style.css",
+  "/login.js",
+  "/language.js",
+  "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
